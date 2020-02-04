6dfd7e957978f44b22697939654edb2277209f09
000sumit <sumitchdhr6@gmail.com>
2020-02-04 22:29:13 +0530

    Scoreboard screen ui and functionality finished

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
-
+//
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
