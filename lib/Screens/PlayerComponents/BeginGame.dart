b35d000a1b7e7fdfccc81148c00c0329bf975f4e
000sumit <sumitchdhr6@gmail.com>
2020-03-06 12:23:16 +0530

    Viewdrone main and some changes

---
+++ lib/Screens/PlayerComponents/BeginGame.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+ class BeginGame extends StatefulWidget {
+   @override
+   _BeginGameState createState() => _BeginGameState();
+ }
+ 
+ class _BeginGameState extends State<BeginGame> {
+   @override
+   Widget build(BuildContext context) {
+     return MaterialApp(
+
+
+
+     );
+   }
+ }
--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-       home:Payment_Details()
+       home:ViewDroneMain()
      );
   }
 }
